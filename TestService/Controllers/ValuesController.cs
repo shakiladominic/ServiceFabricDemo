b4e525abaec8f85ce2b10d289240ca1b7aa6f1fd
ShakilaJoseph <shakila.joseph@ratesetter.com>
2020-02-19 11:58:43 +0000

    first commit

---
+++ TestService/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Data;
+
+namespace TestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        private readonly IReliableStateManager stateManager;
+
+        public ValuesController(IReliableStateManager stateManager)
+        {
+            this.stateManager = stateManager;
+        }
+        // GET: api/Values
+        [HttpGet]
+        public async Task<List<KeyValuePair<string,int>>> Get()
+        {
+            return  new List<KeyValuePair<string, int>>();
+        }
+
+
+    }
+}
